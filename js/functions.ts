b7f3c9a64334bc83d838fbd822e8171b813e6e9f
Roman Shamov <deadsher@mail.ru>
2023-06-17 00:58:48 +0500

    Создание скрипта для генерации описаний постов со случайными ID

---
+++ js/functions.ts
@@ -0,0 +1,23 @@
+const isShorterThan = ({length}: string, maxLength: number): boolean => (length <= maxLength);
+
+const isPalindrom = (str: any): boolean => {
+	const clearedString = str.toLowerCase().replaceAll(' ', '');
+
+	return clearedString === clearedString.split('').reverse().join('');
+};
+
+const getNumbersFromString = (str: string | number): number => {
+	str = str.toString();
+
+	let result = '';
+
+	for (const char of str) {
+		const isNumber = !Number.isNaN(Number.parseInt(char, 10));
+		if (isNumber) {
+			result += char;
+		}
+	}
+
+	// NaN выведется в случае пустой строки на входе
+	return Number(result) || NaN;
+};
---
+++ js/main.ts
@@ -0,0 +1,75 @@
+const MESSAGES: string[] = [
+	'Всё отлично!',
+	'В целом всё неплохо. Но не всё.',
+	'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+	'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+	'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+	'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES: string[] = [
+	'Серега Серый',
+	'Саня Белый',
+	'Никитос Черный',
+	'Гошан Фиолетовый',
+	'Леха Бордовый',
+	'Максон Зеленый',
+	'Дэнчик Желтый',
+	'Ромчик Пурпурный'
+];
+
+const enum Default {
+	SIMILAR_PHOTO_DESCRIPTIONS_COUNT = 25,
+}
+
+const getRandomInteger = (a: number, b: number) : number => {
+	const lower: number = Math.ceil(Math.min(a, b));
+	const upper: number = Math.floor(Math.max(a, b));
+	const result: number = Math.random() * (upper - lower + 1) + lower;
+	return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements: Array<any>): any => elements[getRandomInteger(0, elements.length - 1)];
+
+const createRandomIdFromRangeGenerator = (min: number, max: number): any => {
+	const previousValues: number[] = [];
+
+	if (previousValues.length >= (max - min + 1)) {
+		return null;
+	}
+
+	return function(): number {
+		let currentValue: number = getRandomInteger(min, max);
+		while (previousValues.includes(currentValue)) {
+			currentValue = getRandomInteger(min, max);
+		}
+		previousValues.push(currentValue);
+		return currentValue;
+	};
+};
+
+const generateRandomDescriptionId = createRandomIdFromRangeGenerator(1, 25);
+const generateRandomPhotoId = createRandomIdFromRangeGenerator(1, 25);
+const generateRandomCommentId = createRandomIdFromRangeGenerator(1, 800);
+const generateRandomIdForTest = createRandomIdFromRangeGenerator(1, 35);
+
+const createComment = (): object => ({
+	id: generateRandomCommentId(),
+	avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+	message: getRandomArrayElement(MESSAGES),
+	name: getRandomArrayElement(NAMES)
+});
+
+const createDescription = (): object => ({
+	id: generateRandomDescriptionId(),
+	url: `photos/${generateRandomPhotoId()}.jpg`,
+	description: 'Скоро тут появится полноценное описание!',
+	likes: getRandomInteger(15, 200),
+	comments: Array.from({length: getRandomInteger(0, 30)}, createComment),
+
+});
+
+const similarPhotoDescriptions = Array.from({length: Default.SIMILAR_PHOTO_DESCRIPTIONS_COUNT}, createDescription);
+const generatedIdsForTest = Array.from({length: 35}, generateRandomIdForTest);
+
+export {createRandomIdFromRangeGenerator};
