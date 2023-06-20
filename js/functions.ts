500959ba42221defc810a51251a15d6d11df8520
shori <112016241+dedsher@users.noreply.github.com>
2023-06-21 02:40:58 +0500

    Больше деталей (#4)
    * Добавляет несколько функций для теста
    
    * Удаляет консоль логи
    
    * Редактирует работу eslint
    
    * Вносит правки в функции
    
    * Вносит правки в функции
    
    * Изменяет объявление функций
    
    * Создание скрипта для генерации описаний постов со случайными ID
    
    * Update js/functions.ts
    
    Co-authored-by: baileys-li <47776594+baileys-li@users.noreply.github.com>
    
    * Редактирует functions.ts и index.ts
    
    * Добавляет интерфейсы для объектов
    
    * Убирает вывод в консоль
    
    * Дополняет список комментаторов
    
    * Правит логику в main.ts и functions.ts
    
    ---------
    
    Co-authored-by: baileys-li <47776594+baileys-li@users.noreply.github.com>

---
+++ js/functions.ts
@@ -0,0 +1,17 @@
+const enum Default {
+	MAX_STRING_LENGTH = 140
+}
+const isShorterThan = ({length}: string, maxLength: Default.MAX_STRING_LENGTH): boolean => (length <= maxLength);
+
+const isPalindrome = (str: string): boolean => {
+	const clearedString = str.toLowerCase().replaceAll(' ', '');
+
+	return clearedString === clearedString.split('').reverse().join('');
+};
+
+const getNumbersFromString = (input: number | string) => {
+	const stringWithOnlyDigits = String(input).replace(/\D/g, '');
+	return parseInt(stringWithOnlyDigits, 10);
+};
+
+export { isShorterThan, isPalindrome, getNumbersFromString };
