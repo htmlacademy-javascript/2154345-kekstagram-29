f749d9f9c22f6e66328ebeea8df6cdb9e0424dbd
Роман Шамов <deadsher@mail.ru>
2023-06-23 20:17:21 +0500

    Добавляет функцию сравнения временных отрезков и тесты

--- js/functions.ts
+++ js/functions.ts
@@ -1,7 +1,3 @@
-interface Time {
-	[hours: number]: number
-}
-
 const enum Default {
 	MAX_STRING_LENGTH = 140
 }
@@ -19,8 +15,6 @@ const getNumbersFromString = (input: number | string) => {
 	return parseInt(stringWithOnlyDigits, 10);
 };
 
-const isNumberInTimeRange = (time: number[], bottom = [0, 0], top = [24, 0]) => (bottom <= time && time <= top);
-
 const parseTimeToNumbers = (time: string) => {
 	const result: number[] = [];
 
@@ -31,12 +25,32 @@ const parseTimeToNumbers = (time: string) => {
 	return result;
 };
 
+const isArrayLargerThan = (firstArr: number[], secondArr: number[]) => {
+	const differenceOfHours = firstArr[0] - secondArr[0];
+
+	if (differenceOfHours > 0) {
+		return true;
+	} else if (differenceOfHours === 0) {
+		return firstArr[1] >= secondArr[1];
+	} else {
+		return false;
+	}
+};
+
+const isNumberInTimeRange = (bottom = [0, 0], time: number[], top = [24, 0]) => (isArrayLargerThan(time, bottom) && isArrayLargerThan(top, time));
+
 const addingMinutes = (time: number[], duration: number) => [time[0] + Math.floor(duration / 60), time[1] + duration % 60];
 
 const isTimeSpanOutOfBounds = (workStartTime: string, workEndTime: string, meetingStartTime: string, meetingDurationTime: number) => {
 	const parsedWorkStartTime = parseTimeToNumbers(workStartTime);
 	const parsedWorkEndTime = parseTimeToNumbers(workEndTime);
 	const parsedMeetingStartTime = parseTimeToNumbers(meetingStartTime);
+	const parsedMeetingEndTime = addingMinutes(parsedMeetingStartTime, meetingDurationTime);
+
+	const startMeeting = isNumberInTimeRange(parsedWorkStartTime, parsedMeetingStartTime, parsedMeetingEndTime);
+	const endMeeting = isNumberInTimeRange(parsedMeetingStartTime, parsedMeetingEndTime, parsedWorkEndTime);
+
+	return (startMeeting && endMeeting);
 };
 
-export { isShorterThan, isPalindrome, getNumbersFromString };
+export { isShorterThan, isPalindrome, getNumbersFromString, isTimeSpanOutOfBounds };
--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,5 @@
 import { createMocks } from './mock.ts';
-import { isShorterThan, isPalindrome, getNumbersFromString } from './functions.ts';
+import { isShorterThan, isPalindrome, getNumbersFromString, isTimeSpanOutOfBounds } from './functions.ts';
 import './photo-edit.ts';
 import './photo-upload.ts';
 import './submit-data.ts';
@@ -8,3 +8,4 @@ createMocks();
 isShorterThan('123', 123);
 isPalindrome('топот');
 getNumbersFromString('2033 год');
+isTimeSpanOutOfBounds('08:00', '17:30', '14:00', 90);
