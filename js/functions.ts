3943db5db351e488b3cff243420f49e0ed309252
Роман Шамов <deadsher@mail.ru>
2023-06-23 15:18:32 +0500

    Добавляет функцию сравнения временных отрезков

--- js/functions.ts
+++ js/functions.ts
@@ -1,3 +1,7 @@
+interface Time {
+	[hours: number]: number
+}
+
 const enum Default {
 	MAX_STRING_LENGTH = 140
 }
@@ -13,6 +17,26 @@ const isPalindrome = (str: string): boolean => {
 const getNumbersFromString = (input: number | string) => {
 	const stringWithOnlyDigits = String(input).replace(/\D/g, '');
 	return parseInt(stringWithOnlyDigits, 10);
+};
+
+const isNumberInTimeRange = (time: number[], bottom = [0, 0], top = [24, 0]) => (bottom <= time && time <= top);
+
+const parseTimeToNumbers = (time: string) => {
+	const result: number[] = [];
+
+	time.split(':').forEach((timePiece) => {
+		result.push(Number(timePiece));
+	});
+
+	return result;
+};
+
+const addingMinutes = (time: number[], duration: number) => [time[0] + Math.floor(duration / 60), time[1] + duration % 60];
+
+const isTimeSpanOutOfBounds = (workStartTime: string, workEndTime: string, meetingStartTime: string, meetingDurationTime: number) => {
+	const parsedWorkStartTime = parseTimeToNumbers(workStartTime);
+	const parsedWorkEndTime = parseTimeToNumbers(workEndTime);
+	const parsedMeetingStartTime = parseTimeToNumbers(meetingStartTime);
 };
 
 export { isShorterThan, isPalindrome, getNumbersFromString };
