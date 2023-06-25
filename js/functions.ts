fddb8f949b14d4f569cff58b032ef829f217bc69
shori <112016241+dedsher@users.noreply.github.com>
2023-06-25 19:59:07 +0500

    Функции возвращаются (#6)
    * Добавляет функцию сравнения временных отрезков
    
    * Добавляет функцию сравнения временных отрезков и тесты
    
    * Редактирует подключение functions.ts в index.html
    
    * Минимизирует логику функций functions.ts

--- js/functions.ts
+++ js/functions.ts
@@ -15,4 +15,32 @@ const getNumbersFromString = (input: number | string) => {
 	return parseInt(stringWithOnlyDigits, 10);
 };
 
-export { isShorterThan, isPalindrome, getNumbersFromString };
+const parseTimeToNumbers = (time: string) => time.split(':').map(Number);
+
+const isArrayLargerThan = (firstArr: number[], secondArr: number[]) => {
+	const differenceOfHours = firstArr[0] - secondArr[0];
+
+	if (differenceOfHours) {
+		return differenceOfHours > 0;
+	}
+
+	return firstArr[1] >= secondArr[1];
+};
+
+const isNumberInTimeRange = (bottom = [0, 0], time: number[], top = [24, 0]) => (isArrayLargerThan(time, bottom) && isArrayLargerThan(top, time));
+
+const addingMinutes = (time: number[], duration: number) => [time[0] + Math.floor(duration / 60), time[1] + duration % 60];
+
+const isTimeSpanOutOfBounds = (workStartTime: string, workEndTime: string, meetingStartTime: string, meetingDurationTime: number) => {
+	const parsedWorkStartTime = parseTimeToNumbers(workStartTime);
+	const parsedWorkEndTime = parseTimeToNumbers(workEndTime);
+	const parsedMeetingStartTime = parseTimeToNumbers(meetingStartTime);
+	const parsedMeetingEndTime = addingMinutes(parsedMeetingStartTime, meetingDurationTime);
+
+	const startMeeting = isNumberInTimeRange(parsedWorkStartTime, parsedMeetingStartTime, parsedMeetingEndTime);
+	const endMeeting = isNumberInTimeRange(parsedMeetingStartTime, parsedMeetingEndTime, parsedWorkEndTime);
+
+	return (startMeeting && endMeeting);
+};
+
+export { isShorterThan, isPalindrome, getNumbersFromString, isTimeSpanOutOfBounds };
--- js/main.test.ts
+++
@@ -1,9 +0,0 @@
-import {describe, it, expect} from 'vitest';
-import {createRandomIdFromRangeGenerator} from './main.ts';
-describe('Функция для генерации уникальных ID.', () => {
-	const NUMBER_OF_REQUIRED_ID = 35;
-	const generateRandomIdForTest = createRandomIdFromRangeGenerator(1, NUMBER_OF_REQUIRED_ID);
-	const generatedIdsForTest = Array.from({length: 35}, generateRandomIdForTest);
-
-	it('Проверка на соответствие количества сгенерированных id заданному количеству', () => expect(generatedIdsForTest.length).toBeCloseTo(NUMBER_OF_REQUIRED_ID));
-});
