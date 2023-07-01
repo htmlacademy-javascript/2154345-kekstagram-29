963001c2492a680c57b595a6788e2c5de02ce736
Роман Шамов <deadsher@mail.ru>
2023-07-01 17:50:24 +0500

    Удаляет модуль functions

--- js/main.ts
+++ js/main.ts
@@ -1,7 +1,2 @@
 import './render-data.js';
-import { isShorterThan, isPalindrome, getNumbersFromString, isTimeSpanOutOfBounds } from './functions.ts';
 
-isShorterThan('123', 123);
-isPalindrome('топот');
-getNumbersFromString('2033 год');
-isTimeSpanOutOfBounds('08:00', '12:00', '09:00', 90);
