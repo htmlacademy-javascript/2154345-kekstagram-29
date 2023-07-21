2c278283adce56f99899f07945e0e7bca5f5aff9
Роман Шамов <deadsher@mail.ru>
2023-07-21 21:13:24 +0500

    Попытка вубрать debounce

--- js/filters/filters.ts
+++ js/filters/filters.ts
@@ -1,11 +1,13 @@
 import { Photo } from '../contracts/common';
 import { renderThumbnails } from '../posts/render-thumbnails';
-import { getRandomIdPack } from '../utils';
+import { getRandomIdPack, debounce } from '../utils';
 
 const filters = document.querySelector('.img-filters')!;
 const defaultButton = filters.querySelector('#filter-default')!;
 const randomButton = filters.querySelector('#filter-random')!;
 const discussedButton = filters.querySelector('#filter-discussed')!;
+
+const RERENDER_DELAY = 500;
 
 function compareComments({ comments: commentsA }: Photo, { comments: commentsB }: Photo) {
 	return commentsB.length - commentsA.length;
@@ -46,12 +48,12 @@ const initFilters = (photos: Photo[]) => {
 	filters.classList.remove('img-filters--inactive');
 
 	const onFilterButtonClick = (evt: Event) => {
+		toggleActiveState(evt);
 		const currentElement = evt.target as HTMLButtonElement;
 		const currentFilterName = currentElement.getAttribute('id')!.split('-')[1];
 		const currentFilter = filterOptions[currentFilterName as keyof typeof filterOptions];
 		const currentPhotos = currentFilter(photos);
 		document.querySelectorAll('.picture').forEach((el) => el.remove());
-		toggleActiveState(evt);
 		renderThumbnails(currentPhotos);
 	};
 
--- js/utils.ts
+++ js/utils.ts
@@ -65,4 +65,22 @@ const toggleModalClasses = (wrapper: HTMLElement, willBeOpened = true) => {
 	document.body.classList.toggle('modal-open', willBeOpened);
 };
 
-export {getRandomInteger, getRandomArrayElement, isEscapeKey, findTemplate, findBEMElement, renderPack, toggleModalClasses, getRandomIdPack};
+function debounce (callback, timeoutDelay = 500) {
+	// Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+	// к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+	let timeoutId;
+
+	return (...rest) => {
+		// Перед каждым новым вызовом удаляем предыдущий таймаут,
+		// чтобы они не накапливались
+		clearTimeout(timeoutId);
+
+		// Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+		timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+		// Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+		// пока действие совершается чаще, чем переданная задержка timeoutDelay
+	};
+}
+
+export {getRandomInteger, getRandomArrayElement, isEscapeKey, findTemplate, findBEMElement, renderPack, toggleModalClasses, getRandomIdPack, debounce};
