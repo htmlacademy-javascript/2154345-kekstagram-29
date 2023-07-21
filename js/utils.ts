f0e42e4d307618c7d494932fe5d35416e36123a8
Роман Шамов <deadsher@mail.ru>
2023-07-22 01:43:47 +0500

    Проверка работы

--- js/utils.ts
+++ js/utils.ts
@@ -65,21 +65,13 @@ const toggleModalClasses = (wrapper: HTMLElement, willBeOpened = true) => {
 	document.body.classList.toggle('modal-open', willBeOpened);
 };
 
-function debounce (callback, timeoutDelay = 500) {
-	// Используем замыкания, чтобы id таймаута у нас навсегда приклеился
-	// к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
-	let timeoutId;
+function debounce (callback: () => void, timeoutDelay = 500) {
+	let timeoutId: number;
 
 	return (...rest) => {
-		// Перед каждым новым вызовом удаляем предыдущий таймаут,
-		// чтобы они не накапливались
 		clearTimeout(timeoutId);
 
-		// Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
 		timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-
-		// Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
-		// пока действие совершается чаще, чем переданная задержка timeoutDelay
 	};
 }
 
