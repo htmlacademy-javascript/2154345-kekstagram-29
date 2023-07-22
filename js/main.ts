ec011c899bc5068f88352546244cfa1376379f38
Роман Шамов <deadsher@mail.ru>
2023-07-22 23:01:52 +0500

    Модуль загрузки выбранного изображения

--- js/main.ts
+++ js/main.ts
@@ -1,9 +1,9 @@
 import { renderThumbnails } from './posts/render-thumbnails';
 import { getData } from './fetch/api';
 import { showStatus } from './fetch/status';
-import './photo-upload/form';
 import { setFilters } from './filters/set-filters';
 import { debounce } from './utils';
+import './photo-upload/form';
 
 const RERENDERER_DELAY = 500;
 
